0aa35a23d66a46fd1558155157412bde784b27d9
yupseunghan <sky690212@gmail.com>
2024-12-30 17:31:05 +0900

    isEven과 ++로 센스

---
+++ java_sy/src/day01/Ex12_EvenNumber.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class Ex12_EvenNumber {
+
+	public static void main(String[] args) {
+		int num =3;
+		boolean isEven=num%2==0;
+		System.out.println(num+"은 짝수 인가? "+isEven);
+		num++;
+		System.out.println(num+"는 짝수 인가?"+isEven);
+	}
+
+}
