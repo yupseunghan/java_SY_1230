793febfae812e6cd95d5b3440654d6209db05bf1
yupseunghan <sky690212@gmail.com>
2024-12-30 16:14:42 +0900

    Create Ex06_VariableString.java

---
+++ java_sy/src/day01/Ex06_VariableString.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class Ex06_VariableString {
+
+	public static void main(String[] args) {
+		String str1="Hello World", str2="", str3="A",str4=null;
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		System.out.println(str4);
+		
+		char ch1 ='1';
+		char op ='+';
+		char ch2 = '2';
+		System.out.println(ch1+op+ch2);
+		System.out.println(""+ch1+op+ch2);
+	}
+
+}
