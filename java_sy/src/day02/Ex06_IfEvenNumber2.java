5cb448fa64b070b97f104a3ceae3058fcba73bef
yupseunghan <sky690212@gmail.com>
2024-12-31 17:04:03 +0900

    Create Ex06_IfEvenNumber2.java

---
+++ java_sy/src/day02/Ex06_IfEvenNumber2.java
@@ -0,0 +1,9 @@
+package day02;
+
+public class Ex06_IfEvenNumber2 {
+	public static void main(String[] args) {
+		int num =3;
+		if(num%2==0) System.out.println("짝수");
+		else System.out.println("홀수");
+	}
+}
