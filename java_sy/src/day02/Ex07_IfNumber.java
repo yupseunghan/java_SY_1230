0545cd4c0ed22beb95e6168fcaa9553fb196532a
yupseunghan <sky690212@gmail.com>
2024-12-31 17:04:05 +0900

    Create Ex07_IfNumber.java

---
+++ java_sy/src/day02/Ex07_IfNumber.java
@@ -0,0 +1,10 @@
+package day02;
+
+public class Ex07_IfNumber {
+	public static void main(String[] args) {
+		int num=0;
+		if(num>0) System.out.println("양수");
+		else if(num==0) System.out.println("0이여");
+		else System.out.println("음수");
+	}
+}
