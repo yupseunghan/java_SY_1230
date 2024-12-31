5d5feb1e11f0f708e2e14f4e0dbba925071e404b
yupseunghan <sky690212@gmail.com>
2024-12-31 10:28:33 +0900

    Update Ex12_EvenNumber.java

--- java_sy/src/day01/Ex12_EvenNumber.java
+++ java_sy/src/day01/Ex12_EvenNumber.java
@@ -1,5 +1,5 @@
 package day01;
-
+//변수 활용
 public class Ex12_EvenNumber {
 
 	public static void main(String[] args) {
@@ -9,5 +9,4 @@ public class Ex12_EvenNumber {
 		num++;
 		System.out.println(num+"는 짝수 인가?"+isEven);
 	}
-
 }
