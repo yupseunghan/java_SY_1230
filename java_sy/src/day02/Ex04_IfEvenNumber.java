dae40dcea216c7f6ca2ac69c8cf28301b1555019
yupseunghan <sky690212@gmail.com>
2024-12-31 17:04:00 +0900

    Create Ex04_IfEvenNumber.java

---
+++ java_sy/src/day02/Ex04_IfEvenNumber.java
@@ -0,0 +1,10 @@
+package day02;
+public class Ex04_IfEvenNumber {
+
+	public static void main(String[] args) {
+		int num =7;
+		if(num%2==0) System.out.println(num+"은(는) 짝수");
+			else System.out.println(num+"은(는) 홀수");
+	}
+
+}
