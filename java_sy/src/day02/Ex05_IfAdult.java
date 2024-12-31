6184bd2411e159152459063608469544a7b51fb6
yupseunghan <sky690212@gmail.com>
2024-12-31 17:04:02 +0900

    Create Ex05_IfAdult.java

---
+++ java_sy/src/day02/Ex05_IfAdult.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Ex05_IfAdult {
+
+	public static void main(String[] args) {
+		int age=20;
+		if(age>=20) System.out.println("성인");
+		if(age <20) System.out.println("미성년자");
+	}
+
+}
