7041ad55237e171e75aa64d2cfb723d8bf4d6e89
yupseunghan <sky690212@gmail.com>
2025-03-17 12:49:57 +0900

    서버에서 화면으로 데이터 전송 및 설명

--- spring/framework/spring_h/src/main/java/kr/kh/spring/HomeController.java
+++ spring/framework/spring_h/src/main/java/kr/kh/spring/HomeController.java
@@ -1,37 +1,46 @@
 package kr.kh.spring;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Handles requests for the application home page.
- */
+/*@Controller
+ * => HandletMapping에 url을 등록하기 위한 어노테이션
+ * */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	/*@RequestMapping
+	 * => 처리할 URL 정보를 지정하는 어노테이션으로 해당 정보와 일치하는 경우 메소드를 호출하여 실행
+	 * => value : 처리할 URL을 지정
+	 * => method: 리퀘스트 메소드 처리 방식을 지정. GET,POST,PUT,DELETE 등
+	 * 
+	 * @GetMapping
+	 * => @RequestMapping(method = RequestMethod.GET)인 경우 대체할 수 있는 어노테이션
+	 * 
+	 * @PostMapping
+	 * =>@RequestMapping(method = RequestMethod.Post)인 경우 대체할 수 있는 어노테이션
+	 * */
+	//@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/")
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		/*화면에 데이터를 전송하는 방법
+		 * 	-Model 객체를 이용하여 전송
+		 * 	-addAttribute("화면에서 쓸 이름", 데이터);
+		 * */
+		model.addAttribute("name","홍길동");
+		/*컨트롤러가 Dispatcher Servlet(이하 디스패처)에게 home이라는 문자열을 반환
+		 * =>디스패처가 View Resolver(이하 뷰 리졸버)에게 home이라는 문자열을 전달
+		 * =>뷰 리졸버는 설정된 방법에 따라 home을 가공함
+		 * 	=>뷰 리졸버 설정은 servlet-context.xml에 있음
+		 * 	=>기본 뷰 리졸버에 의해 /WEB-INF/view/home.jsp가 완성되어
+		 * 		최종적으로 해당 jsp의 결과 화면을 클라이언트에 전송*/
 		
 		return "home";
 	}
--- spring/framework/spring_h/src/main/webapp/WEB-INF/views/home.jsp
+++ spring/framework/spring_h/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!doctype html>
 <html>
 <head>
 	<title>Home</title>
@@ -8,7 +10,11 @@
 <h1>
 	Hello world!  
 </h1>
+<!-- 
+서버에서 보낸 데이터를 처리할 떄는 $`{화면에서사용할이름}을 이용하여 활용
+-단 주석에서도 인식 되기 때문에 주석에서 조심힘 작성.
+ -->
+<P>  서버에서 보낸 제 이름은 ${name} 입니다. </P>
 
-<P>  The time on the server is ${serverTime}. </P>
 </body>
 </html>
